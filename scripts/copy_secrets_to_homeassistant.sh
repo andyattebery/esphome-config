d197dd05eef3acf59ef6f3525db769ea3398b35f
Andy Attebery <me@andyattebery.com>
2024-04-28 14:22:05 -0500

    added air-gradient-one-02 device

--- scripts/copy_secrets_to_homeassistant.sh
+++ scripts/copy_secrets_to_homeassistant.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env sh
 
+OP_ESPHOME_ITEM_NAME="ESPHome"
+
 esphome_dir="$(dirname $0)/.."
+
+op item get --format json "$OP_ESPHOME_ITEM_NAME" | jq --raw-output '.fields[] | select(has("section")) | (.section.label | gsub("[\\s-]"; "_")) + "_" + (.label | gsub("[\\s-]"; "_")) + ": " + (.value | @sh)' | sort > "$esphome_dir/secrets.yaml"
 
 scp "$esphome_dir/secrets.yaml" homeassistant:config/esphome/
